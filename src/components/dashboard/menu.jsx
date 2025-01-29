62c08f7bab2d7c2f7f5af4f152da8372a2a121f1
Thallys Correia <thallys@uivo.net>
2025-01-29 03:15:11 -0300

    Adiciona componentes Menu e Navbar ao dashboard para melhorar a navegação e a interface do usuário

---
+++ src/components/dashboard/menu.jsx
@@ -0,0 +1,101 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { LogOut, Ellipsis } from "lucide-react";
+import { signOut } from "next-auth/react";
+import { cn } from "@/lib/utils";
+import { getMenuList } from "@/lib/menu-list";
+import { Button } from "@/components/ui/button";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import CollapseMenuButton from "@/components/dashboard/collapse-menu-button";
+import { Tooltip, TooltipTrigger, TooltipContent, TooltipProvider } from "@/components/ui/tooltip";
+
+const Menu = ({ isOpen }) => {
+  const pathname = usePathname();
+  const menuList = getMenuList(pathname);
+
+  return (
+    <ScrollArea className="[&>div>div[style]]:!block">
+      <nav className="mt-8 h-full w-full">
+        <ul className="flex flex-col min-h-[calc(100vh-132px)] lg:min-h-[calc(100vh-104px)] items-start space-y-1 px-2">
+          {menuList.map(({ groupLabel, menus }, index) => (
+            <li key={index} className={cn("w-full", groupLabel && "pt-5")}>
+              {groupLabel && <MenuGroupLabel groupLabel={groupLabel} isOpen={isOpen} />}
+              <MenuItems menus={menus} pathname={pathname} isOpen={isOpen} />
+            </li>
+          ))}
+          <LogoutButton isOpen={isOpen} />
+        </ul>
+      </nav>
+    </ScrollArea>
+  );
+};
+
+// Exibe o nome do grupo ou um tooltip caso o menu esteja fechado
+const MenuGroupLabel = ({ groupLabel, isOpen }) =>
+  isOpen ? (
+    <p className="text-sm font-medium text-muted-foreground px-4 pb-2 max-w-[248px] truncate">{groupLabel}</p>
+  ) : (
+    <TooltipProvider>
+      <Tooltip delayDuration={100}>
+        <TooltipTrigger className="w-full flex justify-center items-center">
+          <Ellipsis className="h-5 w-5" />
+        </TooltipTrigger>
+        <TooltipContent side="right">{groupLabel}</TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  );
+
+// Renderiza os itens do menu, tratando submenus separadamente
+const MenuItems = ({ menus, pathname, isOpen }) =>
+  menus.map(({ href, label, icon: Icon, active, submenus }, idx) => (
+    <div key={idx} className="w-full">
+      {!submenus?.length ? (
+        <TooltipProvider disableHoverableContent>
+          <Tooltip delayDuration={100}>
+            <TooltipTrigger asChild>
+              <Button variant={active ?? pathname.startsWith(href) ? "secondary" : "ghost"} className="w-full justify-start h-10 mb-1" asChild>
+                <Link href={href}>
+                  <span className={cn(isOpen && "mr-4")}>
+                    <Icon size={18} />
+                  </span>
+                  <p className={cn("max-w-[200px] truncate", isOpen ? "translate-x-0 opacity-100" : "-translate-x-96 opacity-0")}>{label}</p>
+                </Link>
+              </Button>
+            </TooltipTrigger>
+            {!isOpen && <TooltipContent side="right">{label}</TooltipContent>}
+          </Tooltip>
+        </TooltipProvider>
+      ) : (
+        <CollapseMenuButton icon={Icon} label={label} active={active ?? pathname.startsWith(href)} submenus={submenus} isOpen={isOpen} />
+      )}
+    </div>
+  ));
+
+// Componente do botão de logout fixado no rodapé do menu
+const LogoutButton = ({ isOpen }) => {
+  const handleLogout = async () => {
+    await signOut({ callbackUrl: "/auth/login" });
+  };
+
+  return (
+    <li className="w-full grow flex items-end">
+      <TooltipProvider disableHoverableContent>
+        <Tooltip delayDuration={100}>
+          <TooltipTrigger asChild>
+            <Button onClick={handleLogout} variant="outline" className="w-full justify-center h-10 mt-5">
+              <span className={cn(isOpen && "mr-4")}>
+                <LogOut size={18} />
+              </span>
+              <p className={cn("whitespace-nowrap", isOpen ? "opacity-100" : "opacity-0 hidden")}>Sair</p>
+            </Button>
+          </TooltipTrigger>
+          {!isOpen && <TooltipContent side="right">Sair</TooltipContent>}
+        </Tooltip>
+      </TooltipProvider>
+    </li>
+  );
+};
+
+export default Menu;
