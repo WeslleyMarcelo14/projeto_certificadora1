8c608971c5ee056933b18d5f85b1e949f3b769b4
Thallys Correia <thallys@uivo.net>
2025-01-29 19:49:08 -0300

    Refactor Button component to use forwardRef directly

--- src/components/ui/button-login.jsx
+++ src/components/ui/button-login.jsx
@@ -1,14 +1,12 @@
-import * as React from "react";
+import { forwardRef } from "react";
 
-const Button = React.forwardRef(({ className = "", ...props }, ref) => {
-  return (
-    <button
-      ref={ref}
-      className={`inline-flex items-center justify-center px-8 py-4 text-black bg-softPink border border-vibrantPink rounded-full transition-all duration-300 ease-in-out hover:bg-vibrantPink hover:text-softPink hover:border-lightPink disabled:opacity-50 disabled:pointer-events-none ${className}`}
-      {...props}
-    />
-  );
-});
+const Button = forwardRef(({ className = "", ...props }, ref) => (
+  <button
+    ref={ref}
+    className={`inline-flex items-center justify-center px-8 py-4 text-black bg-softPink border border-vibrantPink rounded-full transition-all duration-300 ease-in-out hover:bg-vibrantPink hover:text-softPink hover:border-lightPink disabled:opacity-50 disabled:pointer-events-none ${className}`}
+    {...props}
+  />
+));
 
 Button.displayName = "Button";
 
