a724614d7e1059f42cee4bc9c7a149dff58c8e22
Thallys Correia <thallys@uivo.net>
2025-01-29 19:49:48 -0300

    Remover a importação desnecessária do React no componente ScrollArea

--- src/components/ui/scroll-area.jsx
+++ src/components/ui/scroll-area.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { forwardRef } from "react";
+import { forwardRef } from "react";
 import * as ScrollAreaPrimitive from "@radix-ui/react-scroll-area";
 import { cn } from "@/lib/utils";
 
