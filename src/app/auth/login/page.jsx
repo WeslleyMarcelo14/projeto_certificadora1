2bdc7ea917144321098934161e02fb9273b253ca
Thallys Correia <thallys@uivo.net>
2025-01-29 19:47:36 -0300

    Refatore a estrutura do componente da página de login e simplifique o manuseio da imagem de fundo

--- src/app/auth/login/page.jsx
+++ src/app/auth/login/page.jsx
@@ -2,30 +2,25 @@
 
 import { signIn, useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { Button } from "@/components/ui/button-login";
 import Image from "next/image";
 import { FcGoogle } from "react-icons/fc";
 
-export default function Home() {
+const Home = () => {
   const { status } = useSession();
   const router = useRouter();
 
-  // Definição direta da imagem de fundo, já que ela sempre existe
-  const [bgSrc] = useState("/bg.jpg");
-
-  // Caso o usuário já esteja logado, manda pro dashboard
+  // Redireciona usuário autenticado para o dashboard
   useEffect(() => {
-    if (status === "authenticated") {
-      router.push("/dashboard");
-    }
+    if (status === "authenticated") router.push("/dashboard");
   }, [status, router]);
 
   return (
     <main className="h-screen flex items-center justify-center bg-softPink p-10">
       <div className="grid w-full h-full grid-cols-1 md:grid-cols-2 bg-lightPink">
         {/* Seção Esquerda: Info + Login */}
-        <div className="flex flex-col items-center justify-center bg-mediumPink text-softPink p-6">
+        <section className="flex flex-col items-center justify-center bg-mediumPink text-softPink p-6">
           <h1 className="text-5xl font-bold mb-8">Projeto Bons Fluidos</h1>
           <p className="text-lg text-white mb-6 text-center">Bem-vindo ao painel de gerenciamento de doações do projeto Bons Fluidos.</p>
           <p className="text-md text-white mb-6 text-center">
@@ -38,13 +33,15 @@ export default function Home() {
           <a href="https://www.instagram.com/bonsfluidosutfpr/" target="_blank" rel="noopener noreferrer" className="mt-6 text-sm text-lightPink hover:text-white transition-all">
             Acesse o nosso Instagram: @bonsfluidosutfpr
           </a>
-        </div>
+        </section>
 
         {/* Seção Direita: Imagem de fundo */}
-        <div className="relative hidden md:block">
-          <Image className="object-cover" fill src={bgSrc} alt="Imagem de fundo relacionada ao projeto" />
-        </div>
+        <section className="relative hidden md:block">
+          <Image className="object-cover" fill src="/bg.jpg" alt="Imagem de fundo relacionada ao projeto" />
+        </section>
       </div>
     </main>
   );
-}
+};
+
+export default Home;
