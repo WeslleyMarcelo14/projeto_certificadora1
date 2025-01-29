50f209cb34a5d09747868d241905212375081c9a
Thallys Correia <thallys@uivo.net>
2025-01-29 19:50:12 -0300

    Refatorar o hook useSidebar para melhorar a clareza do código e remover comentários desnecessários

--- src/hooks/use-sidebar.js
+++ src/hooks/use-sidebar.js
@@ -1,42 +1,36 @@
-import { create } from "zustand"; // Importa a função create para criar um estado global com Zustand
-import { persist, createJSONStorage } from "zustand/middleware"; // Middleware para persistir o estado no armazenamento local
-import { produce } from "immer"; // Utilizado para manipular o estado de forma imutável
+import { create } from "zustand";
+import { persist, createJSONStorage } from "zustand/middleware";
+import { produce } from "immer";
 
-// Cria o hook `useSidebar` para gerenciar o estado da barra lateral
+// Hook para gerenciar o estado da barra lateral
 export const useSidebar = create(
   persist(
     (set, get) => ({
-      // Estado inicial
-      isOpen: true, // Define se a barra lateral está aberta
-      isHover: false, // Define se o estado de hover está ativo
-      settings: { disabled: false, isHoverOpen: false }, // Configurações da barra lateral
+      isOpen: true,
+      isHover: false,
+      settings: { disabled: false, isHoverOpen: false },
 
-      // Alterna o estado "isOpen" entre aberto e fechado
       toggleOpen: () => set({ isOpen: !get().isOpen }),
-
-      // Atualiza diretamente o estado "isOpen"
       setIsOpen: (isOpen) => set({ isOpen }),
-
-      // Atualiza diretamente o estado "isHover"
       setIsHover: (isHover) => set({ isHover }),
 
-      // Retorna o estado atual da barra lateral com base em condições
+      // Retorna se a barra lateral deve estar aberta
       getOpenState: () => {
-        const { isOpen, isHover, settings } = get(); // Obtém os valores do estado atual
-        return isOpen || (settings.isHoverOpen && isHover); // Retorna verdadeiro se a barra lateral estiver aberta ou em hover com a configuração habilitada
+        const { isOpen, isHover, settings } = get();
+        return isOpen || (settings.isHoverOpen && isHover);
       },
 
-      // Atualiza as configurações da barra lateral de forma imutável
+      // Atualiza as configurações da barra lateral
       setSettings: (settings) =>
         set(
           produce((state) => {
-            state.settings = { ...state.settings, ...settings }; // Mescla as novas configurações com as existentes
+            Object.assign(state.settings, settings);
           })
         ),
     }),
     {
-      name: "sidebar", // Nome da chave para armazenamento no localStorage
-      storage: createJSONStorage(() => localStorage), // Define o localStorage como armazenamento
+      name: "sidebar", // Nome da chave no localStorage
+      storage: createJSONStorage(() => localStorage),
     }
   )
 );
